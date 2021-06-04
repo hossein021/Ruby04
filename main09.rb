4a5b7a8f4dc65eea4e6e5ab87d83ced381806396
Hossein <hosseinsalmani1373@gmail.com>
2021-06-04 12:19:52 +0430

    data,array,symbol,...

---
+++ main09.rb
@@ -0,0 +1,22 @@
+# انواع داده
+ a = 2      #int
+ b = 2.1    #float
+ "Ali"      #string
+
+ # True , False , Nil   #Boolean
+ # Symbol نماد
+
+c= :a_symbol.object_id
+puts c
+
+#========================
+
+# آرایه
+myarray = ["String", 1, true, :symbol, 3.7]
+puts myarray
+
+# =======================
+
+# هش
+x = {"One"=>"yek", "Two"=>"do"}
+puts x
